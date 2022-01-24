23da66966f0bdd4d11192ff899f61d84bb44266d
Yash Jain <yashsandeepjain2412@gmail.com>
2022-01-24 18:43:12 +0530

    this is new commit

--- diff.cpp
+++ diff.cpp
@@ -4,6 +4,7 @@ int main(){
     int a = 15;
     int b = 6;
     int  diff = a-b;
-    cout<<"sum of the two number is "<<diff;
+    cout<<"sum of the two number is "<<diff<<endl;
+    cout<<"this is new sentence in this file"<<endl;
     return 0;
 }
